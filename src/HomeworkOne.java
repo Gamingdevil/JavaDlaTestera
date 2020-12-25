a739db68907de68d09f9b7240aff6be4c4f8fe6d
Gamingdevil <Gamingdevil@users.noreply.github.com>
2020-12-25 23:17:31 +0100

    Update HomeworkOne.java
    I'm an idiot

--- src/HomeworkOne.java
+++ src/HomeworkOne.java
@@ -1,29 +1,38 @@
 public class HomeworkOne {
     public static void main(String[] args) {
 //      1. 4 int variables
-//      2. Check if the sum of two first ints is even or odd
-//      3. Check if the sum of third and four ints is even or odd
+//      2. Check if the sum of two first is even or odd
+//      3. Check if the sum of third and four is even or odd
 //      4. Check if both sums are even or odd
 
         int numOne = 34;
         int numTwo = 3;
         int numThree = 42;
         int numFour = 16;
-
-        if ((numOne + numTwo) % 2 == 0)
-            System.out.println("The sum of " + numOne + " and " + numTwo + " is even." );
-        else
-            System.out.println("The sum of " + numOne + " and " + numTwo + " is odd." );
-
-
-        if ((numThree + numFour) % 2 == 0)
-            System.out.println("The sum of " + numThree + " and " + numFour + " is even.");
-        else
-            System.out.println("The sum of " + numThree + " and " + numFour + " is odd.");
-
-        if ((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0)
+        
+        boolean sumOne = sumEvenOrOdd(numOne,numTwo);
+        boolean sumTwo = sumEvenOrOdd(numThree,numFour);
+        
+        if(sumOne && sumTwo) {
             System.out.println("Both sums are even.");
-        else
+        } else if (!sumOne && !sumTwo) {
             System.out.println("Both sums are odd.");
+        } else {
+            System.out.println("The sums are not both even or odd.");
         }
+    }
+    
+    private static boolean sumEvenOrOdd(int sum) {
+        int sum = first + second;
+        string resultText = "The sum of " + first + " and " + second + " is ";
+        boolean evenOrOdd = sum % 2 == 0;
+        if (evenOrOdd) {
+            resultText += "even";
+            System.out.println(resultText + "even.");
+        } else {            
+            resultText += "odd.";
+        }
+        System.out.println(resultText);
+        return evenOrOdd;
+    }
 }
